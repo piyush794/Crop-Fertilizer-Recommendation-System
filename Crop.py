094698819cf83594bb79962c490b19ca6c2939e5
PIYUSH NAUTIYAL <nautiyalpiyush54@gmail.com>
2025-02-09 23:56:30 +0530

    Project Completed with PPt

--- Crop.py
+++ Crop.py
@@ -1,1 +1,12 @@
-print("Hello World ")
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import  numpy as np
+crop = pd.read_csv("dataset/Crop_recommendation.csv")
+num_cols_crop = crop.select_dtypes(include=np.number)
+
+# Visualizing with HeatMap
+corr= num_cols_crop.corr()
+# Visualizing with Heatmap
+sns.heatmap(corr, annot=True, cmap='coolwarm')
+plt.show()
